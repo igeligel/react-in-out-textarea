8d87802e4ba3aa084cf9b8838ce58f16a5191cc4
igeligel <kevinigeligeligel@gmail.com>
2020-10-29 23:32:15 +0100

    refactor: IconContainer
    move out styles for IconContainer

---
+++ src/styled/IconContainer.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const IconContainer = styled.div`
+  cursor: pointer;
+  color: rgb(190, 196, 210);
+
+  :hover {
+    color: rgba(107, 120, 146, 1);
+  }
+`;
