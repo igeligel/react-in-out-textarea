842ad4f36dedffd1788d91d7613ffdaf6a1aa804
Emma Dawson <emma.l.dawson@gmail.com>
2021-10-02 19:45:26 +0200

    Remove unused variables

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { IconCopy } from './IconCopy';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { InMenuOptionStuff } from './InMenuOptionStuff';
 import { OutMenuOptionStuff } from './OutMenuOptionStuff';
-import { IInOption, InOptions, IOutOption, OutOptions } from './types';
+import { InOptions, OutOptions } from './types';
 import { CaseBar } from './CaseBar';
 import { SideBar } from './SideBar';
 import { TextAreaContentTop, TextAreaContentBottom } from './TextAreaContent';
