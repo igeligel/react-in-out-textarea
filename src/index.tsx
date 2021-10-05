bfcda1e0538a5177c1bab7afd1ab6e749bcfb88b
Ben Tillman <ben.tillman@gmail.com>
2021-10-05 14:37:12 +1300

    Add key prop for in and out menu options

--- src/index.tsx
+++ src/index.tsx
@@ -123,9 +123,10 @@ export const InOutTextarea: FC<Props> = props => {
                 if (a.active) return -1;
                 return 0;
               })
-              .map(option => {
+              .map((option) => {
                 return (
                   <InMenuOptionStuff
+                    key={option.name}
                     inOptionsMenuRefSizes={inOptionsMenuRefSizes}
                     liveMeasure={liveMeasure}
                     menuOptions={menuInOptions}
@@ -152,9 +153,10 @@ export const InOutTextarea: FC<Props> = props => {
                 if (a.active) return -1;
                 return 0;
               })
-              .map(option => {
+              .map((option) => {
                 return (
                   <OutMenuOptionStuff
+                    key={option.name}
                     outOptionsMenuRefSizes={outOptionsMenuRefSizes}
                     liveMeasure={liveMeasure}
                     menuOptions={menuOutOptions}
